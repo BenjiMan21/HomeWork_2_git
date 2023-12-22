ec6f0e3708b714178f26c8d7bb55d80c9a417f28
Alex Nikiforov <87565892+BenjiMan21@users.noreply.github.com>
2023-12-22 16:52:05 +0400

    Update Main.java

--- src/main/java/com/benjiman21/Main.java
+++ src/main/java/com/benjiman21/Main.java
@@ -2,6 +2,6 @@ package com.benjiman21;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println("Hello qa.guru!!!!!");
     }
 }
